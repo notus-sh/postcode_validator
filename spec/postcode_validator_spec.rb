1b692b543a4b99c9628c390ae7ecd8f6a49867d4
Gaël-Ian <gael-ian@notus.sh>
2023-02-19 18:31:49 -0500

    Fix tests

--- spec/postcode_validator_spec.rb
+++ spec/postcode_validator_spec.rb
@@ -6,7 +6,8 @@ describe ::PostcodeValidator do
   describe '#valid?' do
     it 'returns true for a valid zipcode' do
       countries.each do |country, postcodes|
-        postcodes['valids'].shuffle.each do |postcode|
+        postcodes['valids'].each do |postcode|
+          puts [country, postcode].inspect unless subject.valid?(postcode, country: country)
           expect(subject.valid?(postcode, country: country)).to be_truthy
         end
       end
@@ -14,7 +15,7 @@ describe ::PostcodeValidator do
 
     it 'returns false for an invalid zipcode' do
       countries.each do |country, postcodes|
-        postcodes['invalids'].shuffle.each do |postcode|
+        postcodes['invalids'].each do |postcode|
           expect(subject.valid?(postcode, country: country)).to be_falsey
         end
       end
